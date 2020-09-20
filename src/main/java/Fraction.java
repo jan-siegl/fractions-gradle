dc8642a80ef101854f065bf9399f08faa7e14f46
ephicz <42580612+ephicz@users.noreply.github.com>
2020-09-20 21:40:32 +0200

    pocitani zlomku a testing done

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -3,6 +3,13 @@ public class Fraction implements IFraction {
     private final Integer numerator;
     private final Integer denominator;
 
+    public static void main(String[] args) {
+        Fraction fr1 = new Fraction(1, 3);
+        Fraction fr2 = new Fraction(2, 6);
+        System.out.println("Fraction " + fr1.plus(fr2).getNumerator() + "/" + fr1.plus(fr2).getDenominator());
+        System.out.println(fr2.createNormalised(fr2.getNumerator(), fr2.getDenominator()));
+    }
+
     public Fraction(Integer numerator, Integer denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
@@ -22,30 +29,66 @@ public class Fraction implements IFraction {
 
     @Override
     public IFraction plus(IFraction other) {
-        throw new UnsupportedOperationException();
+        int lcm = commonD(denominator, other.getDenominator());
+        int n1 = lcm / denominator * numerator;
+        int n2 = lcm / other.getDenominator() * other.getNumerator();
+
+        return createNormalised(n1 + n2, lcm);
     }
 
     @Override
     public IFraction minus(IFraction other) {
-        throw new UnsupportedOperationException();
+        int lcm = commonD(denominator, other.getDenominator());
+        int n1 = lcm / denominator * numerator;
+        int n2 = lcm / other.getDenominator() * other.getNumerator();
+
+        return createNormalised(n1 - n2, lcm);
+    }
+    private int commonD(int i, int i1) {
+        if (i == 0 || i1 == 0) {
+            return 0;
+        }
+        int absNumber1 = Math.abs(i);
+        int absNumber2 = Math.abs(i1);
+        int absHigherNumber = Math.max(absNumber1, absNumber2);
+        int absLowerNumber = Math.min(absNumber1, absNumber2);
+        int d = absHigherNumber;
+        while (d % absLowerNumber != 0) {
+            d += absHigherNumber;
+        }
+        return d;
     }
 
     @Override
     public IFraction times(IFraction other) {
-        throw new UnsupportedOperationException();
+        return createNormalised(numerator * other.getNumerator(), denominator * other.getDenominator());
     }
 
     @Override
     public IFraction dividedBy(IFraction other) {
-        throw new UnsupportedOperationException();
+        return createNormalised(numerator * other.getDenominator(), denominator * other.getNumerator());
     }
 
-    public static Fraction createNormalised(Integer numerator, Integer denominator) {
-        throw new UnsupportedOperationException();
+    private static Fraction createNormalised(Integer numerator, Integer denominator) {
+        int lcd = commonD2(numerator, denominator);
+
+        while(lcd > 1){
+            numerator /= lcd;
+            denominator /= lcd;
+            lcd = commonD2(numerator, denominator);
+        }
+
+        return new Fraction(numerator, denominator);
+    }
+    private static int commonD2(int i, int i1) {
+        if (i1 == 0) {
+            return i;
+        }
+        return commonD2(i1, i % i1);
     }
 
     @Override
     public String toString() {
-        return "Fraction " + numerator + "|" + denominator;
+        return "Fraction " + numerator + "/" + denominator;
     }
 }
